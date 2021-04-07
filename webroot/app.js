2f3c92df883dbd935d920a370cc41ead71aeb0fb
Rinon TENDRINOMENA <rinon.ten@onja.org>
2021-04-07 14:29:33 +0300

    Creating all the necessary folders for the app

---
+++ webroot/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+export default function App() {
+    return (
+        <React.Fragment> 
+            <div className="page__container main_container">
+               
+            </div>
+        </React.Fragment>
+    )
+}
